9df8851b84fa87910b30486267ce4fafdff397ec
EgorovGA <egorov.ga@minc.tech>
2024-03-13 01:18:34 +0300

    separate dockerfile in 2 - wine python and builder

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,1 +1,3 @@
-docker build -t dockerwine2 -f compilation/Dockerfile . && docker run -it dockerwine2
+docker build -t wine_python -f compilation/Dockerfile_wine_python . && \
+    docker build -t medhand_build -f compilation/Dockerfile_build .
+    docker run -it medhand_build
