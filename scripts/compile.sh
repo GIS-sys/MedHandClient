acbcceed4e48bb14e3a68109f7a925e9c45c12f3
EgorovGA <egorov.ga@minc.tech>
2024-03-13 03:41:47 +0300

    add copying artefact from docker image

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,8 @@
+# build
 docker build -t wine_python -f compilation/Dockerfile_wine_python . && \
-    docker build -t medhand_build -f compilation/Dockerfile_build .
-    docker run -p 8080:8080 -it medhand_build
+docker build -t medhand_build -f compilation/Dockerfile_build . && \
+# fetch artefact
+docker create -it --name=medhand medhand_build && \
+docker cp medhand:/src/medhand/dist ./target
+# run
+docker run -p 8080:8080 -it medhand_build
