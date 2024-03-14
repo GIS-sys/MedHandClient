aacd07888407d0529a94e4190f4ff51fa79f44e6
EgorovGA <egorov.ga@minc.tech>
2024-03-14 04:18:05 +0300

    dont include zip, automatically create zip

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,5 +5,8 @@ docker build -t medhand_build -f compilation/Dockerfile_build . && \
 (docker container rm medhand || echo "") && \
 docker create -it --name=medhand medhand_build && \
 docker cp medhand:/src/medhand/dist ./target
+# zip artefact
+rm target.zip
+zip -r target.zip target/
 # run
 docker run -p 8080:8080 -it medhand_build
