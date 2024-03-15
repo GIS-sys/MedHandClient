447337c5dcfe1d62be69b15604ae1a903094dda9
EgorovGA <egorov.ga@minc.tech>
2024-03-15 09:48:28 +0300

    all builds in target

--- scripts/compile_ubuntu.sh
+++ scripts/compile_ubuntu.sh
@@ -2,12 +2,13 @@
 docker build -t ubuntu_pyinstaller -f compilation/ubuntu.pyinstaller.Dockerfile . && \
 docker build -t ubuntu_medhand_build -f compilation/ubuntu.build.Dockerfile . && \
 # fetch artefact
-(rm -r ./target_ubuntu || echo "") && \
-(docker container rm ubutnu_medhand || echo "") && \
-docker create -it --name=ubutnu_medhand ubuntu_medhand_build && \
-docker cp ubutnu_medhand:/src/medhand/dist ./target_ubuntu
+mkdir -p target/ && \
+(rm -r target/target_ubuntu || echo "") && \
+(docker container rm ubuntu_medhand || echo "") && \
+docker create -it --name=ubuntu_medhand ubuntu_medhand_build && \
+docker cp ubuntu_medhand:/src/medhand/dist/main target/target_ubuntu && \
 # zip artefact
-rm target_ubuntu.zip
-zip -r target_ubuntu.zip target_ubuntu/
+(rm target/target_ubuntu.zip || echo "") && \
+zip -r target/target_ubuntu.zip target/target_ubuntu/
 # run
 # docker run -p 8080:8080 -it ubuntu_medhand_build
--- scripts/compile_wine.sh
+++ scripts/compile_wine.sh
@@ -2,12 +2,13 @@
 docker build -t wine_pyinstaller -f compilation/wine.pyinstaller.Dockerfile . && \
 docker build -t wine_medhand_build -f compilation/wine.build.Dockerfile . && \
 # fetch artefact
-(rm -r ./target_wine || echo "") && \
+mkdir -p target/ && \
+(rm -r target/target_wine || echo "") && \
 (docker container rm wine_medhand || echo "") && \
 docker create -it --name=wine_medhand wine_medhand_build && \
-docker cp wine_medhand:/src/medhand/dist ./target_wine
+docker cp wine_medhand:/src/medhand/dist/main target/target_wine && \
 # zip artefact
-rm target_wine.zip
-zip -r target_wine.zip target_wine/
+(rm target/target_wine.zip || echo "") && \
+zip -r target/target_wine.zip target/target_wine/
 # run
 # docker run -p 8080:8080 -it wine_medhand_build
