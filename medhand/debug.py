808e643324e2adc6a95e8139754cfaa9c7a14846
EgorovGA <egorov.ga@minc.tech>
2024-03-17 05:03:03 +0300

    add debug for drawing plots

---
+++ medhand/debug.py
@@ -0,0 +1,28 @@
+try:
+    exec("imp" + "ort matplotlib.pyplot as plt")
+except:
+    pass
+
+class Debug:
+    THRESHOLD = 100
+    xs = []
+    ys = {}
+
+    @staticmethod
+    def process(data, timestamp):
+        Debug.xs.append(timestamp)
+        for y_name, y_val in data:
+            if y_name not in Debug.ys:
+                Debug.ys[y_name] = []
+            Debug.ys[y_name].append(y_val)
+        if len(Debug.xs) % Debug.THRESHOLD == 0:
+            Debug.plot()
+
+    @staticmethod
+    def plot():
+        try:
+            for y in Debug.ys.values():
+                plt.plot(Debug.xs, y)
+            plt.show()
+        except:
+            pass
--- medhand/main.py
+++ medhand/main.py
@@ -4,6 +4,7 @@ import time
 import uvicorn
 
 from controller import Controller
+from debug import Debug
 from request_data import RequestData
 
 
@@ -21,6 +22,7 @@ webapp.add_middleware(
 @webapp.post("/new_data")
 async def new_data(data: RequestData):
     timestamp = time.time()
+    Debug.process(data, timestamp)
     Controller.process(data, timestamp)
 
 if __name__ == "__main__":
